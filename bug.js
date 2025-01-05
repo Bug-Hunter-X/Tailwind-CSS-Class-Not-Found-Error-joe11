1125466609a7d2fe7f613d15be2d8e7ee15bdba0
BugHunter-X <askzenai@gmail.com>
2025-01-05 16:34:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example showing a Tailwind CSS class that doesn't exist, leading to no styling applied.
+<div class="bg-blue-500 p-4 text-white">
+  This div should have a blue background, but it might not.
+</div>
+```
